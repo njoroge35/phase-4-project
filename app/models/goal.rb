70c82beb1c0bb051220afe12d13fdfc9856e4557
njoroge35 <110379615+njoroge35@users.noreply.github.com>
2022-12-08 09:00:57 +0300

    uploaded wrong file

--- app/models/goal.rb
+++
@@ -1,9 +0,0 @@
-class Goal < ApplicationRecord
-  belongs_to :bookclub_book
-
-  validates :bookclub_book_id, {presence: true }
-  validates :deadline, {presence: true }
-  validates :pages, {presence: true }
-  validates :pages, numericality: true
-  validates :pages, length: { is: 2 }
-end

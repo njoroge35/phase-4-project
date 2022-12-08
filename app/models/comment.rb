4b118557a4712c4fd51c0e5b3ee1e379d1832a6a
njoroge35 <110379615+njoroge35@users.noreply.github.com>
2022-12-08 09:01:16 +0300

    wrong file

--- app/models/comment.rb
+++
@@ -1,8 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :guide_question
-
-  validates :comment, {presence: true}
-  validates :user_id, {presence: true}
-  validates :guide_question_id, {presence: true}
-end
